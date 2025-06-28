0103a2cb7c9ea1206803a35d75bcf0177e36097d
loadigital <95947050+loadigital@users.noreply.github.com>
2025-06-28 11:05:27 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,4 +3,4 @@ const  BASE_URL  =  'https://api.themoviedb.org/3' ;
 const  IMG_URL  =  'https://image.tmdb.org/t/p/w500' ;
 
 const videoLink = "https://video-360p-mo.blogspot.com/p/mov.html?id=";
-const linkSeries = "https://video360p.site/src/page/series.html?id="; 
+const linkSeries = "https://video-360p-mo.blogspot.com/p/ser.html?id="; 
